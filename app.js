e5d6ced99c68aedee072e7145ed11cc39c72dafc
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-12 23:28:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs")
+const mongoose = require("mongoose")
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.set("view engine","ejs")
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true});
+}
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
